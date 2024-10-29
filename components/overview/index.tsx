8f06a12e3e2e0f07f65c192585211fba23664545
Mohamed Mostafa <58625830+Mohamed-Mostafa-Neymax@users.noreply.github.com>
2024-10-29 16:46:51 +0300

    Testing the app in the Ipad.

--- components/overview/index.tsx
+++ components/overview/index.tsx
@@ -4,7 +4,7 @@ import CustomButton from "../ui/button";
 const Overview: React.FC<{ isPopup: boolean; onClosePopup?: () => void; }> = ({ isPopup, onClosePopup }) => {
     return (
         <div className="fadePage">
-            <div className="pt-5 pl-28 mb-7">
+            <div className="pt-9 pl-28 mb-7">
                 <h2 className="text-2xl MontserratBold text-[#018167] mb-10">Using SALT to measure scalp hair loos<sup>1-3</sup></h2>
                 <ul className="list-disc font-semibold text-xs pl-6">
                     <li className="mb-4">The Severity of Alopecia Tool (SALT) score measures hair loss on a scale of 0 (no scalp hair loss) to 100 (complete scalp hair loss)</li>
