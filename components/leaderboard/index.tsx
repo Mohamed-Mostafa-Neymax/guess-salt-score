f568c88e6821b58863a542ba26869f6ca63ea15e
Mohamed Mostafa <58625830+Mohamed-Mostafa-Neymax@users.noreply.github.com>
2024-11-04 09:25:22 +0200

    Adding some animation to the leaderboard and fixing the server issue.

--- components/leaderboard/index.tsx
+++ components/leaderboard/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { motion, AnimatePresence } from 'framer-motion';
 import Image from "next/image";
 
 import Header from "../home/Header";
@@ -7,16 +8,14 @@ import { useEffect, useState } from "react";
 
 const Leaderboard: React.FC = () => {
     const [leaderboard, setLeaderboard] = useState<{ id: number; name: string; score: number; created_at: string; updated_at: string; }[]>([]);
-    const [loadingUsers, setLoadingUsers] = useState<boolean>(false);
 
     useEffect(() => {
         getLeaderboard();
-        const interval = setInterval(() => { getLeaderboard(); }, 12000);
+        const interval = setInterval(() => { getLeaderboard(); }, 10000);
         return () => { clearInterval(interval); }
     }, [])
 
     async function getLeaderboard() {
-        setLoadingUsers(prevState => true);
         const request = await fetch(
             'https://cms-saltscore.blueholding.co.uk/api/leaderboard',
             {
@@ -28,7 +27,6 @@ const Leaderboard: React.FC = () => {
             });
         const response = await request.json();
         setLeaderboard(_prevState => response);
-        setTimeout(() => { setLoadingUsers(prevState => false); }, 2000);
     }
 
     return (
@@ -38,7 +36,36 @@ const Leaderboard: React.FC = () => {
                     <Image src='/images/top-scores.png' width={350} height={100} alt="SALT Top Scores" />
                 </div>
                 <ul className="w-[500px] relative">
-                    {
+                    <AnimatePresence>
+                        {
+                            leaderboard.map((user, index) => (
+                                <motion.div
+                                    key={user.id}
+                                    className={`pl-24 mb-3 flex justify-between ${index < 3 ? 'bg-[#B7D57D]' : 'bg-[#CCE2A1]'}`}
+                                    layout
+                                    initial={{ opacity: 0, y: -20 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: 20 }}
+                                    transition={{ type: 'spring', stiffness: 300, damping: 20 }}>
+                                    <div className="flex items-center w-full text-2xl MontserratBold text-[#013E53]">
+                                        {
+                                            index < 3 ? (
+                                                <Image src={`/images/user${index + 1}.png`} className="absolute -left-8" width={55} height={55} alt="User position" />
+                                            ) : (
+                                                <span>{index + 1}.&nbsp;</span>
+                                            )
+                                        }
+                                        <p>{user.name}</p>
+                                    </div>
+                                    <div className="bg-[#013E53] text-white min-w-[120px] w-[80px] h-[80px] flex flex-col items-center justify-center">
+                                        <h1 className="text-3xl MontserratBold">{user.score}</h1>
+                                        <h1 className="text-md font-bold">POINTS</h1>
+                                    </div>
+                                </motion.div>
+                            ))
+                        }
+                    </AnimatePresence>
+                    {/* {
                         leaderboard.map((user, index) => (
                             <li key={user.id} className={`pl-24 mb-3 flex justify-between ${loadingUsers ? 'animate-pulse' : ''} ${index < 3 ? 'bg-[#B7D57D]' : 'bg-[#CCE2A1]'}`}>
                                 <div className="flex items-center w-full text-2xl MontserratBold text-[#013E53]">
@@ -57,7 +84,7 @@ const Leaderboard: React.FC = () => {
                                 </div>
                             </li>
                         ))
-                    }
+                    } */}
                 </ul>
             </div>
             <Header isLeaderboard={true} />
